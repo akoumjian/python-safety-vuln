6ae5ae6635373800037992db8d91f897e3888afb
Alec Koumjian <akoumjian@gmail.com>
2020-02-17 17:40:05 -0500

    Place paths for multiple versions of python

--- setup.py
+++ setup.py
@@ -5,7 +5,11 @@ setup(
     name="malicious",
     version="0.1",
     packages=find_packages(),
-    data_files=[("lib/python3.6/site-packages/", ["malicious/malicious.pth"])],
+    data_files=[
+        ("lib/python3.6/site-packages/", ["malicious/malicious.pth"]),
+        ("lib/python3.7/site-packages/", ["malicious/malicious.pth"]),
+        ("lib/python3.8/site-packages/", ["malicious/malicious.pth"]),
+    ],
     author="Alec Koumjian",
     author_email="akoumjian@gmail.com",
     description="Avoid detection by tools like safety using load time imports.",
